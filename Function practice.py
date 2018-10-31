2ac11786e20df4217aa68c7f8965e1085c9eaf68
andrew <andrew.poltoranos@greenwoodcollege.com>
2018-10-31 13:38:25 -0400

    First section

--- Function practice.py
+++ Function practice.py
@@ -1,9 +1,12 @@
 import time
 import random
+
 def sleep():
     random_num = random.randint(0,10)
     time.sleep(int(random_num))
     print("BOO!")
+
+
 def trick_or_treat():
     word = random.choice("Trick","Treat")
     print(word)
